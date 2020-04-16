ed8e3977c3b3ba1ed5fd39faf101f41762e9c7f6
Maria Grudinina <mary.gru@mail.ru>
2020-04-16 21:39:05 +0600

    Added SOAP checks for projects tests

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,15 +1,26 @@
 from suds.client import Client
 from suds import WebFault
 
+from model.project import Project
+
 
 class SoapHelper:
     def __init__(self, app):
         self.app = app
+        self.wsdl = "http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl"
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.wsdl)
         try:
             client.service.mc_login(username, password)
             return True
         except WebFault:
             return False
+
+    def get_projects_list(self, username, password):
+        client = Client(self.wsdl)
+        projects = client.service.mc_projects_get_user_accessible(username, password)
+        list_to_return = []
+        for project in projects:
+            list_to_return.append(Project(project_name=project["name"], project_id=project["id"]))
+        return list_to_return
--- test/test_projects.py
+++ test/test_projects.py
@@ -5,25 +5,29 @@ from model.project import Project
 
 
 def test_add_project(app):
-    app.session.login("administrator", "root")
-    old_projects = app.project.get_projects_list()
+    username = "administrator"
+    password = "root"
+    app.session.login(username, password)
+    old_projects = app.soap.get_projects_list(username, password)
     project = Project(project_name="My test project " + str(datetime.now()))
     app.project.create(project)
     old_projects.append(project)
-    new_projects = app.project.get_projects_list()
+    new_projects = app.soap.get_projects_list(username, password)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
     app.session.logout()
 
 
 def test_delete_project(app):
-    app.session.login("administrator", "root")
-    old_projects = app.project.get_projects_list()
+    username = "administrator"
+    password = "root"
+    app.session.login(username, password)
+    old_projects = app.soap.get_projects_list(username, password)
     if len(old_projects) == 0:
         app.project.create(Project(project_name="My test project " + str(datetime.now())))
-        old_projects = app.project.get_projects_list()
+        old_projects = app.soap.get_projects_list(username, password)
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.project_id)
     old_projects.remove(project)
-    new_projects = app.project.get_projects_list()
+    new_projects = app.soap.get_projects_list(username, password)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
     app.session.logout()
